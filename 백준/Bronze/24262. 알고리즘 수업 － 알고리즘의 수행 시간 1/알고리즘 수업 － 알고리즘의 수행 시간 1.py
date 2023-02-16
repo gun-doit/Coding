55f3119dd9ea90a9dd53288b4c6c97572092b7d3
장건수 <80693289+watergun0613@users.noreply.github.com>
2023-02-17 01:45:02 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.py
@@ -0,0 +1,14 @@
+import sys
+import math
+n = int(sys.stdin.readline())
+if n==n:
+    print(n//n)
+    print(0)
+elif n==n**2:
+    print(n//n**2)
+    print(2)
+elif n==n**3:
+    print(n//n**3)
+    print(3)
+else:
+    print(4)
