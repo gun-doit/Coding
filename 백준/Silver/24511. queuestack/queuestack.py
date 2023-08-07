b80fd9b1c63fbd636cc99226cac1cfdfbd84ba78
장건수 <80693289+watergun0613@users.noreply.github.com>
2023-08-07 18:15:04 +0900

    [Silver III] Title: queuestack, Time: 184 ms, Memory: 49208 KB -BaekjoonHub

--- 백준/Silver/24511. queuestack/queuestack.py
+++ 백준/Silver/24511. queuestack/queuestack.py
@@ -5,14 +5,12 @@ input = sys.stdin.readline
 N = int(input()) #자료 구조의 갯수
 QS = list(map(int, input().split()))
 array = list(map(int, input().split()))
-deq = deque(array[i] for i in range(N) if QS[i] == 0)
+queue = [array[i] for i in range(N-1,-1,-1) if QS[i] == 0]
 M = int(input())
 C = list(map(int, input().split()))
 result = []
 
 for num in C:
-    if len(result) == M:
-        break
-    deq.appendleft(num)
-    result.append(deq.pop())
-print(*result)
+    queue.append(num)
+print(*queue[:M])
+
