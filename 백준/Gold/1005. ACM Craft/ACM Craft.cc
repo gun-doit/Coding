fd0b4ea0fcbada4912d036f6139365b4786df21b
장건수 <80693289+watergun0613@users.noreply.github.com>
2023-09-22 13:23:18 +0900

    [Gold III] Title: ACM Craft, Time: 172 ms, Memory: 8132 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.cc
+++ 백준/Gold/1005. ACM Craft/ACM Craft.cc
@@ -1,53 +1,79 @@
-#include <iostream>
-#include<vector>
-#include<string.h>
-#include<queue>
-
+// Level : Gold III
+#include <bits/stdc++.h>
+#define MAX 1001
 using namespace std;
 
+int N, K, T, W;
+int x, y;
+vector<int> Graph[MAX];
+int Dtime[MAX];
+int inDegree[MAX] = {0,};
+int Dp[MAX] = {0,};
+
+struct Step{
+    int w;
+    int t;
+};
+
+void solution(){
+    int result[MAX];
+    queue<int> q;
+    for(int i = 1; i<=N; i++){
+        if(inDegree[i]==0){
+            q.push(i);
+            Dp[i] = Dtime[i];
+        }
+    }
+
+    for(int i = 1; i<=N; i++){
+        if(q.empty()){
+            return;
+        }
+        int x = q.front();
+        q.pop();
+        result[i] = x;
+        for(int j = 0; j< Graph[x].size(); j++){
+            int y = Graph[x][j];
+            if(Dp[y] < Dp[x] + Dtime[y]){
+                Dp[y] = Dp[x] + Dtime[y];
+            }
+            if(--inDegree[y] == 0){
+                q.push(y);
+            }
+        }
+    }
+    
+    cout << Dp[W] << '\n';
+}
+
+
+void Answer(){
+    cin >> T;
+    for(int i = 0; i<T; i++){
+        cin >> N >> K;
+        memset(inDegree,0,MAX*sizeof(int));
+        memset(Graph,0,MAX*sizeof(vector<int>));
+        memset(Dp,0,MAX*sizeof(int));
+        for(int j = 1; j<=N; j++){
+            cin >> Dtime[j];
+        }
+        for(int k = 0; k<K; k++){
+            cin >> x >> y;
+            Graph[x].push_back(y);
+            inDegree[y]++;
+        }
+        cin >> W;
+
+        solution();
+        
+    }
+}
 
 int main(){
-    int T; cin >> T;
-    
-    while(T--){
-        int n,k,w;
-        cin >> n >> k;
 
-        vector<long> spent(n+1,0);
-        vector<int> vec[n+1];
-        vector<int> entry(n+1,0);
-        vector<long> dp(n+1,0);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-        for(int i = 1; i<=n; i++) cin >> spent[i];
-        for(int i = 0; i<k; i++){
-            int a, b; cin >> a >> b;
-            entry[b]++;
-            vec[a].push_back(b);
-        }
-        cin >> w;
-
-        queue<int> q;
-        for(int i = 1; i<=n; i++){
-            if(entry[i] == 0){
-                q.push(i);
-                dp[i] = spent[i];
-            }
-        }
-
-        while(!q.empty()){
-            int t = q.front(); q.pop();
-            int node = t;
-            for(int i =0; i<vec[node].size(); i++){
-                int nxt = vec[node][i];
-                dp[nxt] = max(dp[nxt], dp[node] + spent[nxt]);
-                entry[nxt]--;
-                if(entry[nxt] == 0){
-                    q.push(nxt);
-                }
-            }
-        }
-        cout << dp[w] << "\n";
-
-    }
+    Answer();
     return 0;
 }
