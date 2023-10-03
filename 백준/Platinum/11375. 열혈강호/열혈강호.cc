972fe14b73d284da4d768589449e5c8a801ce164
장건수 <80693289+watergun0613@users.noreply.github.com>
2023-10-03 13:22:58 +0900

    [Platinum IV] Title: 열혈강호, Time: 292 ms, Memory: 6176 KB -BaekjoonHub

---
+++ 백준/Platinum/11375. 열혈강호/열혈강호.cc
@@ -0,0 +1,57 @@
+//Level : Platinum IV
+#include <bits/stdc++.h>
+#define MAX 2001
+using namespace std;
+
+int N, M;
+int A[MAX], B[MAX];
+bool visited[MAX];
+vector<int> adj[MAX];
+
+bool DFS(int a){
+    visited[a] = true;
+    for(int b: adj[a]){
+        if(B[b] == -1 || !visited[B[b]] && DFS(B[b])){
+            A[a] = b;
+            B[b] = a;
+            return true;
+        }
+    }
+    return false;
+}
+
+void solution(){
+    int max_matching = 0;
+    fill(A, A+N, -1);
+    fill(B, B+M+1, -1);
+    for(int i = 0; i<N; i++){
+        if(A[i] == -1){
+            fill(visited, visited+N, false);
+            if(DFS(i)) ++max_matching;
+        }
+    }
+
+    cout << max_matching << '\n';
+}
+
+void Answer(){
+    cin>> N >> M;
+    int k, tmp;
+    for(int i = 0; i< N; i++){
+        cin >> k;
+        for(int j = 0 ; j<k; j++){
+            cin >> tmp;
+            adj[i].push_back(tmp);
+        }
+    }
+    solution();
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+
+    Answer();
+    return 0;
+}
